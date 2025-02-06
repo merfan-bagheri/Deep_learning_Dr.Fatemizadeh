aa90697bab431ca001289b74ca307786ddedb3ed
Mohammad Erfan Bagherinejad <merfan.bagheri00@gmail.com>
2025-02-06 12:20:04 +0330

    Add files via upload

--- Project/utils/video_utils.py
+++ Project/utils/video_utils.py
@@ -4,7 +4,7 @@ import natsort
 
 def save_video(output_video_frames, output_video_path):
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    out = cv2.VideoWriter(output_video_path, fourcc, 25, 
+    out = cv2.VideoWriter(output_video_path, fourcc, 30, 
                           (output_video_frames[0].shape[1], output_video_frames[0].shape[0]))
     for frame in output_video_frames:
         out.write(frame)
